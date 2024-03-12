dcc7122bec64e585dd714322b1d3d3e4444dadba
mvvalerio <martimvv07@gmail.com>
2024-03-12 12:37:36 -0100

    Update

--- form registo/main.js
+++ form registo/main.js
@@ -19,22 +19,34 @@ $(document).ready(function(){
     
     });
 
-    $("#loader").hide();
+    $("#load").hide();
+
+    
+
 
     $("form").on("submit", function(event){
         console.log("submit");
 
+        var username = $("input#username").val();
+        var email = $("input#email").val();
+        var nascimento = $("input#dob").val();
+
+        var frase = "Olá "+username+", com data de nascimento"+dob+", foi registado para o email"+email;
+
+        console.log("frase");
+
         event.preventDefault();
 
-        $("#loader").fadeIn(1200);
+        $("#load").fadeIn(1200, function(){
 
-        $(this).find("span").text("Enviado com sucesso");
+            $(this).find("span").text("Enviado com sucesso");
 
-        $(this).find(".close").on("click", function(){
+            $(this).find(".close").on("click", function(){
             $(this).parent().fadeOut(500);
+            });
         });
     });
-
+});
     //$("#loader").show();
 
     /*       -------------------------OU-------------------------
@@ -45,4 +57,3 @@ $(document).ready(function(){
 
     */
 
-});
